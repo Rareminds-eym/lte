8098319e905519243610205caceb9fcd4f83e302
Hariprasath-rareminds <hariprasath@rareminds.in>
2026-07-24 17:27:48 +0530

    feat: implement lazy-loaded AppRouter, add PageLoader component,

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -4,3 +4,4 @@ export { Button, type ButtonProps } from "./Button";
 export { ErrorFallback } from "./ErrorFallback";
 export { IconButton, type IconButtonProps } from "./IconButton";
 export { Image } from "./Image";
+export { PageLoader } from "./PageLoader";
