8f36f753f58fd34ed595264d52ec15f4379dd047
litikesh-dev <litikesh@rareminds.in>
2026-07-30 13:10:31 +0530

    feat: removed line

--- vite.config.ts
+++ vite.config.ts
@@ -15,10 +15,7 @@ export default defineConfig({
 	],
 	resolve: {
 		tsconfigPaths: true,
-	},
-	optimizeDeps: {
-		exclude: ["@file-viewer/pptx"],
-	},
+  },
 	server: {
 		host: '0.0.0.0',
 		port: 8080,
