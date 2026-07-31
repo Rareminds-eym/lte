5cb56a7052e909c5bbef802222e79cef54c9794b
litikesh-dev <litikesh@rareminds.in>
2026-07-31 09:24:28 +0530

    feat: export StageStepperBar component and related types from new index file

--- src/widgets/StageStepperBar/index.ts
+++ src/widgets/StageStepperBar/index.ts
@@ -1,3 +1,3 @@
 export { STAGE_STEPS } from "./ui/constants";
-export type { StageStepInfo, StageStepperBarProps } from "./ui/types";
 export { StageStepperBar } from "./ui/StageStepperBar";
+export type { StageStepInfo, StageStepperBarProps } from "./ui/types";
