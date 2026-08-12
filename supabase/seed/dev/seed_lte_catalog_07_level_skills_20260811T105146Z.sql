744c8803596db4e5f75d14d2e241b9bb2a526ae1
litikesh-dev <litikesh@rareminds.in>
2026-08-12 10:09:03 +0530

    feat: add database seed files for LTE catalog modules, artifacts, and skill structures in dev

---
+++ supabase/seed/dev/seed_lte_catalog_07_level_skills_20260811T105146Z.sql
@@ -0,0 +1,114 @@
+-- Generated from LTE_LEARNING_CATALOG_CIE_CAP_003_LINKS_COMPLETE.xlsx: public.level_skills.
+-- Edit the Excel template and rerun scripts/excel_to_lte_seed.py.
+-- ON CONFLICT DO NOTHING keeps this seed safe to rerun.
+
+BEGIN;
+
+-- public.level_skills
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('6f2800b0-dc53-5ee0-a662-4562c5412337'::uuid, '029f3de1-70b8-5b3f-9203-6f5cee8be5f5'::uuid, 'f07f2b0a-31b4-5257-9eea-d17436a53ead'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('0c01f3c9-de0a-57f1-a86f-4dace0c24e06'::uuid, '029f3de1-70b8-5b3f-9203-6f5cee8be5f5'::uuid, '941e0186-296a-5fc4-93ef-72f6d90dea93'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('3a20a3b7-2484-599b-b0af-09119d743dcc'::uuid, '029f3de1-70b8-5b3f-9203-6f5cee8be5f5'::uuid, '89b41360-a0d3-5609-bc6c-6ff792779e85'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('ec3521a7-c7c7-5dfb-b013-071a0b1f93cb'::uuid, '029f3de1-70b8-5b3f-9203-6f5cee8be5f5'::uuid, 'f031a1c8-af9c-5d0d-8740-9b79a30b91c1'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('fd14fb3d-7491-5261-8369-37ebaa91995d'::uuid, '029f3de1-70b8-5b3f-9203-6f5cee8be5f5'::uuid, 'eae485f7-8575-5285-9930-d31b0ef33375'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('bb50c7b5-c845-5371-b7c9-6ce7a21c06c4'::uuid, '029f3de1-70b8-5b3f-9203-6f5cee8be5f5'::uuid, '7a7769f1-7f7b-565b-bb3a-d7999b3c3e5d'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('777cde2e-fd49-5544-8383-7f2c9ef40d6b'::uuid, '029f3de1-70b8-5b3f-9203-6f5cee8be5f5'::uuid, 'f16ed2bb-e932-5919-b8c3-551efebbcde1'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('66e21822-7f12-5b6b-b66a-115bed011467'::uuid, '8bfef3e8-d301-5436-8857-7cc9c36456ef'::uuid, '4c5c0c36-2482-5127-b8ba-4ed7c75d4ced'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('8ec4790b-debb-5036-a420-ba8947dcef0c'::uuid, '8bfef3e8-d301-5436-8857-7cc9c36456ef'::uuid, 'be7c2bce-1791-5e1f-9197-7619ff85e650'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('a078eb97-c301-5459-be10-6878e0866593'::uuid, '8bfef3e8-d301-5436-8857-7cc9c36456ef'::uuid, '0792ecc1-d807-5a67-9545-d627a4fdb532'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('07b1a72c-c1ae-5ef6-ac2e-4f65c2ea0fed'::uuid, '8bfef3e8-d301-5436-8857-7cc9c36456ef'::uuid, '9f57ec11-264e-5283-b1c3-f55758ff8094'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('c266b978-0c66-5bc6-b905-62f00af69d1b'::uuid, '8bfef3e8-d301-5436-8857-7cc9c36456ef'::uuid, '550a8efd-bc4f-51a3-94cd-bee9aae959ee'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('3f308d7d-971c-5aae-a8e0-ec68bc6604a8'::uuid, '8bfef3e8-d301-5436-8857-7cc9c36456ef'::uuid, 'f1c38b26-49f1-530f-b231-44c0e6d86a46'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('eb212ca7-eafd-5a49-accf-29034c62501d'::uuid, '8bfef3e8-d301-5436-8857-7cc9c36456ef'::uuid, 'd0f77de7-c122-5e34-8539-1fac6c4b96d0'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('a252b80f-80a5-5771-b15c-c0c6974ba995'::uuid, '97137f7e-f69e-5403-9103-de6dbbabf803'::uuid, '4a74e0da-ad40-5b6a-8e50-0dbca41123a5'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('56515346-9d00-5e34-bc24-7ca795b5a472'::uuid, '97137f7e-f69e-5403-9103-de6dbbabf803'::uuid, '6aebb88d-2430-5c0c-a490-fb636aba69dd'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('be3fb9df-ff42-5ab0-a5ed-0f6a7befb90f'::uuid, '97137f7e-f69e-5403-9103-de6dbbabf803'::uuid, 'f4a3e54d-363c-5fa7-89a8-c08867187008'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('fa44473b-25a3-53a4-bb7d-6f4ab2797044'::uuid, '97137f7e-f69e-5403-9103-de6dbbabf803'::uuid, '20a0ba7d-b7d7-5c72-bf54-8a84957259bb'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('41c0783d-e9a5-50a6-a6b6-60400b525264'::uuid, '97137f7e-f69e-5403-9103-de6dbbabf803'::uuid, 'ccfe9c8b-cc82-53a2-9344-d85d825b17a4'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('e7780439-2328-5f1f-b6a1-63eec5c1c021'::uuid, '97137f7e-f69e-5403-9103-de6dbbabf803'::uuid, '339b0ab5-5252-5baa-9ba4-47d5e169f007'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('8fa442a6-b337-52dc-8122-e12c5f589f32'::uuid, '97137f7e-f69e-5403-9103-de6dbbabf803'::uuid, '80d29452-58b5-5ce3-99dc-eca4f17a3cdb'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('596f0a72-21a8-53e0-8e1f-326841b14e3e'::uuid, 'd40290f1-2a2c-5fe1-822f-cfe353d9f3d4'::uuid, '6496c8ac-84cd-544a-9a44-b12eb808353e'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('d094623e-f5d4-527c-84ab-9c7aa4899f9f'::uuid, 'd40290f1-2a2c-5fe1-822f-cfe353d9f3d4'::uuid, 'db6c143c-f905-5ee5-bcc8-9fce85e1b493'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('4b15c903-036e-5038-93b1-c9fdfc12f41b'::uuid, 'd40290f1-2a2c-5fe1-822f-cfe353d9f3d4'::uuid, '58a4a91b-b081-5ba5-9129-3750fd2cd223'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('b344dec5-784a-579e-b7cf-317ed529666f'::uuid, 'd40290f1-2a2c-5fe1-822f-cfe353d9f3d4'::uuid, '4deb3e71-6995-5675-aece-049c641e1dab'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('9e06dee0-b82b-502e-804b-e3337435f24f'::uuid, 'd40290f1-2a2c-5fe1-822f-cfe353d9f3d4'::uuid, 'aa6e7fc8-5b31-5d6b-a121-a72dbbf76051'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('18a7b6e2-73f0-5c1c-88da-2bd94fdf5468'::uuid, 'd40290f1-2a2c-5fe1-822f-cfe353d9f3d4'::uuid, '7bc2feeb-aa19-5234-9965-696ad109f49a'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('7fbb79e9-797e-538d-b03c-cf85da9b7ca0'::uuid, 'd40290f1-2a2c-5fe1-822f-cfe353d9f3d4'::uuid, '53b58824-9ec5-50e0-9dee-09bde18a423a'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('f7fc2cd5-98a4-56a0-b57d-7cdc2e77452e'::uuid, '0c615ad6-2d07-5f7d-9766-928144f8f194'::uuid, '59c14fb5-0bda-5f60-92a9-a5a6ef5eb78c'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('09afac34-7501-5e3c-8945-735fe33b4cfc'::uuid, '0c615ad6-2d07-5f7d-9766-928144f8f194'::uuid, '72319569-a0f7-5c72-8fd0-fb5d819d2112'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('4d3835ef-ec8c-589f-9948-f40f1348ffe2'::uuid, '0c615ad6-2d07-5f7d-9766-928144f8f194'::uuid, '32e7f364-294d-54de-8476-eef6eabd793f'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('f02a777e-17f2-5334-8579-1b4e341e68c9'::uuid, '0c615ad6-2d07-5f7d-9766-928144f8f194'::uuid, '21c86829-1a85-5638-bdcf-e82bc7653643'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('97e25eaf-0610-5536-b52e-4d4e733046dc'::uuid, '0c615ad6-2d07-5f7d-9766-928144f8f194'::uuid, 'a19cd5d4-4f3c-5ce2-bc33-ed06f7bcc43f'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('0e0e6ea0-e102-5774-9b1f-6e7e328b9697'::uuid, '0c615ad6-2d07-5f7d-9766-928144f8f194'::uuid, '28375bef-5089-5621-8758-25ee8b3fa132'::uuid)
+ON CONFLICT DO NOTHING;
+INSERT INTO public."level_skills" ("id", "level_id", "skill_id")
+VALUES ('4d3073c8-7586-54a3-acf1-033bdb3cfaee'::uuid, '0c615ad6-2d07-5f7d-9766-928144f8f194'::uuid, 'ff4cd878-3baa-5b4e-966b-45fd0b1aa21c'::uuid)
+ON CONFLICT DO NOTHING;
+
+COMMIT;
